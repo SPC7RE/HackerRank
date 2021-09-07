251394d57364813f341618c0dd41517cabd7a3a8
SPEC7RE <RNCFDs@gmail.com>
2021-09-07 18:26:02 +0000

    Add first challenges.

---
+++ algorithms/SolveMeFirst.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/solve-me-first/submissions/code/81909549
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a + b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
---
+++ python/HelloWorld.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/py-hello-world/problem
+
+def helloWorld():
+	print("Hello World!")
+
+if __name__ == '__main__':
+    helloWorld()
