5718c5e44221d48084d87b2071c9c153dc6eaf4d
Spec7re <RNCFDs@gmail.com>
2021-12-12 13:21:47 +0200

    Count vowels in a string.

---
+++ algorithms/Vowel_Counter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+'''
+Finds a substring with bigest count of vowels, 
+with custom length k, from a string.
+Currently max lenght size that can handle is k = 175 
+'''
+
+# from file
+# with open('test.txt', 'r') as f:
+# 	s = f.read()
+
+def vowelCounter(s, k):
+	# Write your code here
+	vowels = {'a', 'e', 'i', 'o','u'}
+	gen = (s[i:i+k] for i in range(0, len(s)))
+
+	chunks = []
+	for i in gen:
+		chunks.append(i) 
+
+	# print(chunks)	
+
+	countVowels = []
+	for i in range(len(chunks)):
+			countVowels.append(sum([chunks[i].count(x) for x in vowels]))
+
+	index = countVowels.index(max(countVowels))
+
+	if max(countVowels) <= 1:
+			return 'Not found!'
+	else:
+			return chunks[index]
+
+if __name__ == '__main__':
+  s = 'ssssssdkkkkkkksssssaaammmmmmmmeeooouuunnnnnnnniiiaaaooouuueee'
+  k = 16
+  print(vowelCounter(s, k))
