6c6be26825927eeb6c252f70952e480eaf175f19
Spec7re <RNCFDs@gmail.com>
2022-07-24 20:28:54 +0300

    Fix auto indentation.

--- algorithms/Apple&Oranges.py
+++ algorithms/Apple&Oranges.py
@@ -17,13 +17,13 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
 	
 	apples_inbound = 0
 	for i in all_ground_apples:
-			if sam_left_fence <= i and sam_right_fence >= i:
-					apples_inbound += 1
+		if sam_left_fence <= i and sam_right_fence >= i:
+			apples_inbound += 1
 	
 	oranges_inbound = 0
 	for j in all_ground_oranges:
-			if sam_left_fence <= j and j <= sam_right_fence:
-					oranges_inbound += 1
+		if sam_left_fence <= j and j <= sam_right_fence:
+			oranges_inbound += 1
 	
 	print(apples_inbound)
 	print(oranges_inbound)
