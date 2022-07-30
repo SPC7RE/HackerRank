34d4e26b3e216019480c982fadeeca311d895307
Spec7re <RNCFDs@gmail.com>
2022-07-31 00:04:17 +0300

    Split dinner bill function.

---
+++ algorithms/Bill_Division.py
@@ -0,0 +1,43 @@
+#!/bin/env python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    # Write your code here
+    whole_bill = sum(bill)
+    not_eaten = bill.pop(k)
+    splited_bill = int(sum(bill)/2)  
+    anna_paid = b
+    
+    if anna_paid == splited_bill:
+        print('Bon Appetit')
+    else:
+        print(anna_paid - splited_bill)
+    
+    
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
